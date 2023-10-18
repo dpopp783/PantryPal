b6e880527a03a2220454a5db577f220706fc78a4
Daniel29872 <arnholtdaniel@gmail.com>
2023-10-18 17:39:04 -0400

    add separate html source for each section

--- src/app.py
+++ src/app.py
@@ -3,5 +3,21 @@ from flask import *
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html")
+def login():
+    return render_template("dashboard.html")
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/ingredients")
+def ingredients():
+    return render_template("ingredients.html")
+
+@app.route("/recipes")
+def recipes():
+    return render_template("recipes.html")
+
+@app.route("/shoppinglist")
+def shoppinglist():
+    return render_template("shoppinglist.html")
