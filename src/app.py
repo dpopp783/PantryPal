8373b234cf5e7483341c4a56046921da4ce40044
Daniel29872 <arnholtdaniel@gmail.com>
2023-10-24 15:27:46 -0400

    working on getting the inventory view running

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from flask import *
+from inventory_tracker import InventoryTracker
+from inventory_entry import InventoryEntry, Ingredient
+from datetime import date
 
 app = Flask(__name__)
 
@@ -12,7 +15,15 @@ def dashboard():
 
 @app.route("/ingredients")
 def ingredients():
-    return render_template("ingredients.html")
+    test = InventoryTracker()
+
+    item1 = InventoryEntry(Ingredient("Rice", 1), 3, "cup", date(2023, 11, 1))
+    item2 = InventoryEntry(Ingredient("Flour", 2), 4, "cup", date(2023, 12, 1))
+    item3 = InventoryEntry(Ingredient("Cheese", 3), 5, "cup", date(2023, 12, 24))
+
+    test.inventory = [item1, item2, item3]
+
+    return render_template("ingredients.html", data = test.inventory)
 
 @app.route("/recipes")
 def recipes():
