8c9c8682dd1ba06ab8a70beba9494161e5116e3f
RB321 <82303310+RB321@users.noreply.github.com>
2023-11-13 23:40:59 -0500

    recipe_webapp
    recipe front-app

--- src/app.py
+++ src/app.py
@@ -1,48 +1,48 @@
 from flask import *
 
-import config
+# import config
 from inventory_tracker import InventoryTracker
 from inventory_entry import InventoryEntry, Ingredient
 from datetime import date
-import psycopg2
-from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+#import psycopg2
+#from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 import csv
 
-conn = psycopg2.connect(
-    user='postgres',
-    password=config.postgres_pw,
-    host='localhost',
-    dbname='pantry',
-    port=5433)
+# conn = psycopg2.connect(
+#     user='postgres',
+#     password=config.postgres_pw,
+#     host='localhost',
+#     dbname='pantry',
+#     port=5433)
 
 
-# Open a cursor to perform database operations
-cur = conn.cursor()
+# # Open a cursor to perform database operations
+# cur = conn.cursor()
 
-cur.execute('DROP TABLE IF EXISTS ingredient;')
-cur.execute('CREATE TABLE ingredient (id int PRIMARY KEY,'
-            'name varchar(100) NOT NULL);')
+# cur.execute('DROP TABLE IF EXISTS ingredient;')
+# cur.execute('CREATE TABLE ingredient (id int PRIMARY KEY,'
+#             'name varchar(100) NOT NULL);')
 
-with open('src/top-1k-ingredients.csv') as ingredients:
-    reader = csv.reader(ingredients, delimiter=';')
-    for row in reader:
-        ing_name, ing_id = row
-        if ing_name.find("'") != -1:
-            # TODO there is a syntax error if you try to insert a string into the postgresql db that has a ' in it, haven't yet figured out how to escape it
-            continue
-        cur.execute(f"INSERT INTO ingredient(id, name) VALUES({ing_id}, '{ing_name}')")
+# with open('src/top-1k-ingredients.csv') as ingredients:
+#     reader = csv.reader(ingredients, delimiter=';')
+#     for row in reader:
+#         ing_name, ing_id = row
+#         if ing_name.find("'") != -1:
+#             # TODO there is a syntax error if you try to insert a string into the postgresql db that has a ' in it, haven't yet figured out how to escape it
+#             continue
+#         cur.execute(f"INSERT INTO ingredient(id, name) VALUES({ing_id}, '{ing_name}')")
 
-# Create inventory_entry table
-cur.execute('DROP TABLE IF EXISTS inventory_entry;')
-cur.execute('CREATE TABLE inventory_entry (id serial PRIMARY KEY,'
-            'ingredient_id int NOT NULL REFERENCES ingredient(id),'
-            'amount float NOT NULL,'
-            'unit varchar(50) NOT NULL,'
-            'expiration_date date DEFAULT CURRENT_DATE + 7);'
-            )
-cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(20444, 10, 'cup','2023-12-1')")
-cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(20081, 8, 'cup','2023-12-24')")
-cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(1009, 2, 'cup','2023-11-1')")
+# # Create inventory_entry table
+# cur.execute('DROP TABLE IF EXISTS inventory_entry;')
+# cur.execute('CREATE TABLE inventory_entry (id serial PRIMARY KEY,'
+#             'ingredient_id int NOT NULL REFERENCES ingredient(id),'
+#             'amount float NOT NULL,'
+#             'unit varchar(50) NOT NULL,'
+#             'expiration_date date DEFAULT CURRENT_DATE + 7);'
+#             )
+# cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(20444, 10, 'cup','2023-12-1')")
+# cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(20081, 8, 'cup','2023-12-24')")
+# cur.execute("INSERT INTO inventory_entry(ingredient_id, amount, unit, expiration_date) VALUES(1009, 2, 'cup','2023-11-1')")
 
 app = Flask(__name__)
 
@@ -57,29 +57,39 @@ def dashboard():
     return render_template("dashboard.html")
 
 
-@app.route("/ingredients")
+@app.route("/ingredients", methods=["GET"])
 def ingredients():
     test = InventoryTracker()
 
-    cur.execute('SELECT * FROM inventory_entry')
-    entries = cur.fetchall()
+    # cur.execute('SELECT * FROM inventory_entry')
+    # entries = cur.fetchall()
 
     inventory = []
-    for id, ing_id, amount, unit, date in entries:
-        cur.execute(f'SELECT name FROM ingredient WHERE id={ing_id}')
-        ing_name = cur.fetchall()[0][0]
-        inventory.append(InventoryEntry(Ingredient(ing_name, ing_id), amount, unit, date))
+    # for id, ing_id, amount, unit, date in entries:
+    #     cur.execute(f'SELECT name FROM ingredient WHERE id={ing_id}')
+    #     ing_name = cur.fetchall()[0][0]
+    #     inventory.append(InventoryEntry(Ingredient(ing_name, ing_id), amount, unit, date))
 
-    test.inventory = inventory
+    test.inventory = [InventoryEntry(Ingredient("Rice", 1), 1, "cup", "2023-11-01")]
 
     # TODO: Add another arg called inventory_JSON, set it equal to the JSON representation of the InventoryTracker
-    return render_template("ingredients.html", inventory=test.inventory)
+    return render_template("ingredients.html", inventory=test.inventory, inventory_JSON = '{"1":{"ingredient":{"name":"Rice", "id":1}, "quantity":1, "unit":"cup", "expiration_date":"2023-11-01"}}')
 
+@app.route("/ingredients/add", methods=["POST"])
+def ingredients_add():
+    return render_template("ingredients.html")
+
+@app.route("/ingredients/modify", methods=["POST"])
+def ingredients_modify():
+    return render_template("ingredients.html")
+
+@app.route("/ingredients/remove", methods=["POST"])
+def ingredients_remove():
+    return render_template("ingredients.html")
 
 @app.route("/recipes")
 def recipes():
     return render_template("recipes.html")
-
 
 @app.route("/shoppinglist")
 def shoppinglist():
