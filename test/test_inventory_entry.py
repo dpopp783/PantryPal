938b73004e38ae69a733a5fd343fbf51e8e419de
Daniel29872 <arnholtdaniel@gmail.com>
2023-11-13 23:53:12 -0500

    inv entry beginning

--- test/test_inventory_entry.py
+++ test/test_inventory_entry.py
@@ -1,8 +1,14 @@
 import unittest
+from src.inventory_entry import Ingredient
 
 
+def test_initialization():
+    a = Ingredient("Rice", 1)
+    assert a.id == 1 and a.name == "Rice"
 
 
+def test_initialization_err():
+    a = Ingredient("Rice")
 
 
 if __name__ == "__main__":
