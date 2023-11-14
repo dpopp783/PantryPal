e143841a83a2e822d34df19a7ca8853131cacc5b
Daniel29872 <arnholtdaniel@gmail.com>
2023-11-13 21:33:27 -0500

    changed ingredient page to POST requests format

--- src/static/js/ingredients.js
+++ src/static/js/ingredients.js
@@ -1,13 +1,19 @@
 
-$(document).ready( () => {});
+$(document).ready( () => {
+    $("#filter").on(); // TODO: Update
+    $("#search").on("input", search);
 
-var inventory_updates = {};
+    $("#modify-popup").on("show.bs.modal", modify);
+
+    $("#save").on("click", submit);
+    $("#remove").on("click", submit);
+});
 
 function search(){
     let key = $("#search").val().toLowerCase()
 
     $(".item.row").each((i, element) => {
-        if (inventory[element.id] && inventory[element.id].ingredient.name.toLowerCase().includes(key)){
+        if (row.children().eq(0).text().toLowerCase().includes(key)){
             $("#"+element.id).css("display", "flex");
         } else {
             $("#"+element.id).css("display", "none");
@@ -15,131 +21,105 @@ function search(){
     });
 }
 
-function add(){
-    // TODO: Add pop-up modification window
-
-    
-    let item = {} // TODO: Use data from window to fill item obj
-
-    let table = $("#inventory");
-    table.append(
-        `<div class="item row py-1 pe-1 border-top border-dark-subtle bg-opacity-25 bg-success" id="`+ item.ingredient.id + `" style="align-items: center;">
-            <div class="col-4">`+ item.ingredient.name + `</div>
-            <div class="col-4">`+ item.quantity + ` ` + item.unit + `</div>
-            <div class="col-3">`+ item.expiration_date + `</div>
-            <div class="col-1">
-                <div class="btn-group">
-                    <button class="btn btn-warning border-0" title="Modify Ingredient" name="modify">
-                        <i class="bi bi-pencil-square"></i>
-                    </button>
-                    <button class="btn btn-danger border-0" title="Remove Ingredient" name="remove">
-                        <i class="bi bi-trash3-fill"></i>
-                    </button>
-                </div>
-            </div>
-        </div>`
-    );
-
-    table.find("[name='modify']").on("click", modify);
-    table.find("[name='remove']").on("click", remove);
+function filter(){
+    // TODO: Add filter functionality
 }
 
-function reset(){
-    let table = $("#inventory");
-    table.children().remove();
+function modify(event){
+    let id = Number($(event.relatedTarget).data("id"));
 
-    for (let id in inventory) {
-        table.append(
-            `<div class="item row py-1 pe-1 border-top border-dark-subtle bg-opacity-25" id="`+ inventory[id].ingredient.id + `" style="align-items: center;">
-                <div class="col-4">`+ inventory[id].ingredient.name + `</div>
-                <div class="col-4">`+ inventory[id].quantity + ` ` + inventory[id].unit + `</div>
-                <div class="col-3">`+ inventory[id].expiration_date + `</div>
-                <div class="col-1">
-                    <div class="btn-group">
-                        <button class="btn btn-warning border-0" title="Modify Ingredient" name="modify">
-                            <i class="bi bi-pencil-square"></i>
-                        </button>
-                        <button class="btn btn-danger border-0" title="Remove Ingredient" name="remove">
-                            <i class="bi bi-trash3-fill"></i>
-                        </button>
-                    </div>
-                </div>
-            </div>`
-        );
+    if (id) {
+        let item = inventory[id];
+        $("#id").val(id);
+        $("#name").val(item.ingredient.name);
+        $("#quantity").val(item.quantity);
+        $("#unit").val(item.unit);
+        $("#expiration_date").val(item.expiration_date);
     }
-
-    table.find("[name='modify']").on("click", modify);
-    table.find("[name='remove']").on("click", remove);
-
-    inventory_updates = {}
-    $("#filter").val(""); // TODO: Update
-    $("#search").val("");
-
-    toastr.info("Reset changes...")
 }
 
-function save(){
-    // TODO: Add code to post request to back-end
-}
+function submit(){
+    let form = $("#modify-form");
+    let button = $(this);
 
-function modify(){
-    let row = $(this).closest(".item.row")
-    let id = Number(row.attr("id"));
-    
-    // TODO: Add pop-up modification window
-
-    let item = inventory[id];
-    let item_updates = {
-        ingredient: {
-            name: row.children().eq(0).text(),
-            id: id
-        },
-        quantity: Number(row.children().eq(1).text().split(" ")[0]),
-        unit: row.children().eq(1).text().split(" ")[1],
-        expiration_date: row.children().eq(2).text()
-    };
-
-    if (row.hasClass("bg-success")) {
-        inventory_updates[id] = item_updates;
-    } else if (
-        item.ingredient.id != item_updates.ingredient.id ||
-        item.quantity != item_updates.quantity ||
-        item.unit != item_updates.unit ||
-        item.expiration_date != item_updates.expiration_date
-    ) {
-        inventory_updates[id] = item_updates;
-        row.addClass("bg-warning");
+    if (button.attr("id") == "save") {
+        let id = Number($("#id"));
+        if (inventory[id]){
+            form.attr("action", "/ingredients/modify")  
+        } else {
+            form.attr("action", "/ingredients/add")  
+        }
     } else {
-        delete inventory_updates[id];
-        row.removeClass("bg-warning");
+        form.attr("action", "/ingredients/remove")  
     }
 
+    form.submit();
+    form.reset();
 }
 
 function remove(){
-    let row = $(this).closest(".item.row");
-    let id = Number(row.attr("id"));
+    let form = $("#modify-form");
+    form.attr("action", "/ingredients/remove")
+    form.submit();
+    form.reset();
 
-    if (row.hasClass("bg-success")) {
-        delete inventory_updates[id];
-        row.remove();
-        return;
-    } else if (row.hasClass("bg-danger")) {
-        delete inventory_updates[id];
-        row.removeClass("bg-danger");
-        row.find("[name='modify']").prop("disabled", false);
-    } else {
-        inventory_updates[id] = {};
-        row.addClass("bg-danger");
-        row.find("[name='modify']").prop("disabled", true);
-    }
 }
 
+// let item_updates = {
+//     ingredient: {
+//         name: "", //TO-DO: fix
+//         id: $("#m-ingredient").val()
+//     },
+//     quantity: $("#m-quantity").val(),
+//     unit: $("#m-unit").val(),
+//     expiration_date: $("#m-expiration_date").val()
+// };
+// console.log(item_updates)
 
-$("#filter").on(); // TODO: Update
-$("#search").on("input", search);
-$("#add").on("click", add);
-$("#reset").on("click", reset);
-$("#save").on("click", save);
-$("[name='modify']").on("click", modify);
-$("[name='remove']").on("click", remove);
+// let item = inventory[id];
+// let item_updates = {
+//     ingredient: {
+//         name: row.children().eq(0).text(),
+//         id: id
+//     },
+//     quantity: Number(row.children().eq(1).text().split(" ")[0]),
+//     unit: row.children().eq(1).text().split(" ")[1],
+//     expiration_date: row.children().eq(2).text()
+// };
+
+// if (row.hasClass("bg-success")) {
+//     inventory_updates[id] = item_updates;
+// } else if (
+//     item.ingredient.id != item_updates.ingredient.id ||
+//     item.quantity != item_updates.quantity ||
+//     item.unit != item_updates.unit ||
+//     item.expiration_date != item_updates.expiration_date
+// ) {
+//     inventory_updates[id] = item_updates;
+//     row.addClass("bg-warning");
+// } else {
+//     delete inventory_updates[id];
+//     row.removeClass("bg-warning");
+// }
+
+// let table = $("#inventory");
+// table.append(
+//     `<div class="item row py-1 pe-1 border-top border-dark-subtle bg-opacity-25 bg-success" id="`+ item.ingredient.id + `" style="align-items: center;">
+//         <div class="col-4">`+ item.ingredient.name + `</div>
+//         <div class="col-4">`+ item.quantity + ` ` + item.unit + `</div>
+//         <div class="col-3">`+ item.expiration_date + `</div>
+//         <div class="col-1">
+//             <div class="btn-group">
+//                 <button class="btn btn-warning border-0" title="Modify Ingredient" name="modify">
+//                     <i class="bi bi-pencil-square"></i>
+//                 </button>
+//                 <button class="btn btn-danger border-0" title="Remove Ingredient" name="remove">
+//                     <i class="bi bi-trash3-fill"></i>
+//                 </button>
+//             </div>
+//         </div>
+//     </div>`
+// );
+
+// table.find("[name='modify']").on("click", modify);
+// table.find("[name='remove']").on("click", remove);
