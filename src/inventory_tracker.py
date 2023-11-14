d5866d5990cce7d5c20bf8875b8864d31de9d14c
dpopp783 <dpopp783@gmail.com>
2023-11-13 20:26:13 -0500

    create add_entry method

--- src/inventory_tracker.py
+++ src/inventory_tracker.py
@@ -11,5 +11,8 @@ class InventoryTracker:
     def __str__(self):
         return ",".join(map(lambda i: i.name, self.inventory))
 
+    def add_entry(self, ie: InventoryEntry):
+        self.inventory.append(ie)
+
     def jsonify(self):
         return json.dumps({ie.ingredient.id: ie.jsonify() for ie in self.inventory})
