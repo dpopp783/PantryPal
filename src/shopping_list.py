2cdfcda9ffec3cf3b74e96a116da44f2c193793a
dpopp783 <dpopp783@gmail.com>
2023-11-14 02:42:33 -0500

    change DS to dictionary, create add_item method

--- src/shopping_list.py
+++ src/shopping_list.py
@@ -1,12 +1,15 @@
 from inventory_entry import InventoryEntry
-from typing import List
+from typing import Dict
 import json
 
 
 class ShoppingList:
 
     def __init__(self):
-        self.shopping_list: List[InventoryEntry] = []
+        self.shopping_list: Dict[str, InventoryEntry] = dict()
+
+    def add_item(self, item: InventoryEntry):
+        self.shopping_list[str(item.ingredient.id)] = item
 
     def jsonify(self):
-        return json.dumps({ie.ingredient.id: ie.jsonify() for ie in self.shopping_list})
+        return json.dumps({ie.ingredient.id: ie.to_dict() for ie in self.shopping_list.values()})
